947358b4a6f0d2fba7a2662ee5138c51059ed31e
DCastrillon0531 <DCastrillon0531@users.noreply.github.com>
2020-02-28 20:06:58 -0500

    Index4 Continuacion Taller

--- index.php
+++ index.php
@@ -39,12 +39,34 @@ echo("<br>");
 $peso=85;
 $estatura=1.78;
 $imc=$peso/$estatura;
+echo("Señor usuario su peso es: ".$peso." y su estatura es: ".$estatura);
 
 if($imc<=18.4){
     echo("Insuficiencia de peso");
 }
-elseif($imc<=18.5 || $imc<=24.9){
+else{
+if($imc<=18.5 || $imc<=24.9){
     echo("Peso normal");
+}else{
+    if($imc<=25 || $imc<=29.9){
+        echo("Sobrepeso");
+    }
+    else{
+        if($imc<=30 || $imc<=34.9){
+            echo("Obesidad Tipo 1");
+        }
+        else{
+            if($imc<=35 || $imc<=39.9){
+                echo("Obesidad Tipo 2");
+            }
+            else{
+                if($imc>=40){
+                    echo("Obesidad Tipo 3");
+                }
+            }
+        }
+    }
+}
 }
 
 ?>
